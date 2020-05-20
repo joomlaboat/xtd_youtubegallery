601eaea560ff1c1363a3853740d017a941ead447
JoomlaBoat <ivankomlev@gmail.com>
2020-05-20 13:58:20 -0500

    Initial commit

---
+++ youtubegallerybutton.php
@@ -0,0 +1,40 @@
+<?php
+
+ // no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.plugin.plugin' );
+
+class plgButtonYoutubeGalleryButton extends JPlugin
+{
+
+    public function onDisplay($name)
+	{
+        $css = ".icon-YoutubeGalleryButton {
+                    background: transparent url('/plugins/editors-xtd/youtubegallerybutton/youtube-logo.png') no-repeat 0 4px !important;
+                }";
+
+        $doc = JFactory::getDocument();
+        $doc->addStyleDeclaration($css);
+        $button = new JObject();
+        $button->set('modal', true);
+        $button->set('text', JText::_('Youtube Gallery')); //PLG_YOUTUBEGALLERYBUTTON_BUTTON_YOUTUBEGALLERY - fix it
+        $button->set('name', 'YoutubeGalleryButton');
+        $button->set('id', 'YoutubeGalleryButton');
+        
+        $app = JFactory::getApplication();
+
+		if($app->isSite())
+            $button->set('link', '/administrator/index.php?option=com_youtubegallery&amp;view=listandthemeselection&amp;tmpl=component&amp;e_name='.$name);
+        else
+            $button->set('link', '/index.php?option=com_youtubegallery&amp;view=listandthemeselection&amp;tmpl=component&amp;e_name='.$name);
+            
+            
+        $button->options = "{handler: 'iframe', size: {x: 800, y: 500}, id:'ygb', name:'ygbn'}";
+        
+        
+        
+        return $button;
+	}
+    
+}
