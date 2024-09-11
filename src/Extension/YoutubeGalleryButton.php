f9743a2a668f88508ac7d6862e31150ae4e0bb2e
joomlaboat <ivankomlev@gmail.com>
2024-09-10 20:07:32 -0500

    Version Changed

--- src/Extension/YoutubeGalleryButton.php
+++ src/Extension/YoutubeGalleryButton.php
@@ -19,7 +19,7 @@ use Joomla\CMS\Uri\Uri;
 // phpcs:enable PSR1.Files.SideEffects
 
 /**
- * Editor Pagebreak button
+ * Editor YoutubeGallery button
  *
  * @since  1.5
  */
@@ -67,36 +67,7 @@ final class YoutubeGalleryButton extends CMSPlugin //implements SubscriberInterf
         $button->iconSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
   <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" fill="red" />
 </svg>';
-        /*
-        $button->options = [
-            'height'     => '200px',
-            'width'      => '400px',
-            'bodyHeight' => '70',
-            'modalWidth' => '80',
-        ];*/
-
 
         return $button;
     }
-/*
-    public static function getSubscribedEvents(): array
-    {
-       return ['onEditorButtonsSetup' => 'onEditorButtonsSetup'];
-    }
-
-    public function onEditorButtonsSetup(EditorButtonsSetupEvent $event)
-    {
-        $subject = $event->getButtonsRegistry();
-        $disabled = $event->getDisabledButtons();
-
-        if (\in_array($this->_name, $disabled)) {
-            return;
-        }
-
-        $this->loadLanguage();
-
-        $button = $this->onDisplay($event->getEditorId());
-        $subject->add($button);
-    }
-*/
 }
