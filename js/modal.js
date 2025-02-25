f423f8dfb6a16883609bd1986bbdae9d27925b49
joomlaboat <ivankomlev@gmail.com>
2025-02-25 11:17:31 -0900

    Version Changed. CT Lib Updated. Article Editor: Insert button fixed.

--- js/modal.js
+++ js/modal.js
@@ -1,31 +1,37 @@
-import {JoomlaEditor} from 'editor-api';//, JoomlaEditorButton
-//import JoomlaDialog from 'joomla.dialog';
-
 window.YG = {
-
     initialize: function () {
         const o = this.getUriObject(window.self.location.href),
             q = this.getQueryObject(o.query);
 
         this.frameurl = location.href;
-
         this.editor = q.e_name;
     },
 
     close: function () {
-        window.parent.Joomla.Modal.getCurrent().close();
+        if (window.parent.Joomla && window.parent.Joomla.Modal) {
+            window.parent.Joomla.Modal.getCurrent().close();
+        }
     },
 
     insert: function (videoList, theme) {
-
         const tag = '{youtubegalleryid=' + videoList + ',' + theme + '}';
 
-        if (window.Joomla.editors) {
-            const instances = JoomlaEditor.instances;
-            const editorInstance = JoomlaEditor.get('jform_articletext');
-            editorInstance.replaceSelection(tag);
-            window.parent.Joomla.Modal.getCurrent().close();
+        if (window.Joomla && window.Joomla.editors) {
+            let editorInstance;
+
+            if (typeof JoomlaEditor !== "undefined" && JoomlaEditor.get) {
+                // Joomla 5
+                editorInstance = JoomlaEditor.get('jform_articletext');
+            } else if (window.Joomla.editors.instances) {
+                // Joomla 4
+                editorInstance = window.Joomla.editors.instances['jform_articletext'];
+            }
+
+            if (editorInstance) {
+                editorInstance.replaceSelection(tag);
+                this.close();
+            }
         }
         return true;
     },
-}
+};
